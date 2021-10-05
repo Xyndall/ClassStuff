43400c2ce8e722b29afa2b7b8b4e63e6c6bc508e
tyrel <tyrel.ratima@gmail.com>
2021-10-05 15:22:44 +1300

    Ai ++ scaryGame
    first person player movement
    AI movement

---
+++ Assets/Scripts/Ai.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Ai : MonoBehaviour
+{
+    //AI
+    NavMeshAgent agent = null;
+    public float speed = 5;
+    GameObject TargetPoint = null;
+
+    //
+    public LayerMask mask;
+    public float raycastDist = 10;
+    public GameObject hitTarget = null;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        TargetPoint = new GameObject("TargetPoint");
+        //hitTarget = TargetPoint;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        //Vector3 direction = new Vector3(Mathf.Sin(Time.time), 0, 1);
+        //agent.Move(direction * Time.deltaTime * speed);
+
+        RaycastHit hit;
+        Debug.DrawRay(transform.position, transform.forward * raycastDist, Color.magenta);
+        if (Physics.Raycast(transform.position, transform.forward, out hit, raycastDist, mask))
+        {
+            hitTarget = hit.transform.gameObject;
+            
+        }
+        agent.SetDestination(hitTarget.transform.position);
+    }
+
+
+}
--- Assets/Scripts/Raycast.cs
+++ Assets/Scripts/Raycast.cs
@@ -4,8 +4,13 @@ using UnityEngine;
 
 public class Raycast : MonoBehaviour
 {
+    public objBreak objDestroy = null;
 
     public LayerMask mask;
+
+    float horizontal = 0;
+    float vertical = 0;
+    public float speed = 10;
 
     // Start is called before the first frame update
     void Start()
@@ -16,6 +21,14 @@ public class Raycast : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+
+        vertical = Input.GetAxis("Vertical");
+        horizontal = Input.GetAxis("Horizontal");
+
+        Vector3 move = new Vector3(horizontal, 0, vertical);
+
+        gameObject.transform.position += move * Time.deltaTime * speed;
+
         if (Input.GetKey(KeyCode.Mouse0))
         {
             RaycastHit hit;
@@ -23,6 +36,8 @@ public class Raycast : MonoBehaviour
             if (Physics.Raycast(transform.position, transform.forward, out hit, 10, mask))
             {
                 Debug.Log(hit.transform.name);
+                //Destroy(gameObject);
+                objDestroy.GetComponent<objBreak>().DestroyObj();
             }
         }
 
